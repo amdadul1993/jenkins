09056b1c36a13560ee2dcaff8f20e4bb14ac3ee6
amdadul1993 <74498722+amdadul1993@users.noreply.github.com>
2022-01-30 22:58:53 +0600

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "5. please visit my website learning-ocean.com";
+        return "My Name is Khan and I am no terrorist";
     }
 
 }
